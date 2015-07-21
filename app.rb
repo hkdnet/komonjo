4983a3537b1e120fb1a2b293bc0f4a5cc9e8ff33
hkdnet <hkdnet@users.noreply.github.com>
2015-07-22 02:00:38 +0900

    index shows a dummy list

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 require 'slim'
+require 'slim/include'
+require_relative 'lib/mock/slack_mock'
 
 module Komonjo
   # routing
   class App < Sinatra::Base
     get '/' do
+      @posts = Komonjo::Mock::SlackMock.chat_logs
       slim :index
     end
   end
---
+++ lib/mock/slack_mock.rb
@@ -0,0 +1,30 @@
+module Komonjo
+  module Mock
+    # Mock for SlackAdapter
+    class SlackMock
+      class << self
+        def chat_logs
+          {
+            'messages' => [
+              { 'text' => 'hoge', 'username' => 'bot', 'type' => 'message',
+                'subtype' => 'bot_message', 'ts' => '1437406342.000007' },
+              { 'type' => 'message', 'user' => 'User01', 'text' => 'ほう',
+                'ts' => '1437405883.000006' },
+              { 'text' => 'TEST from slack-api', 'username' => 'bot',
+                'type' => 'message', 'subtype' => 'bot_message',
+                'ts' => '1437405875.000005' },
+              { 'type' => 'message', 'user' => 'User02', 'text' => 'fuga',
+                'ts' => '1437405463.000004' },
+              { 'type' => 'message', 'user' => 'User02', 'text' => 'hoge',
+                'ts' => '1437405463.000003' },
+              { 'user' => 'User01', 'type' => 'message',
+                'subtype' => 'channel_join',
+                'text' => '<@User01|Username01> has joined the channel',
+                'ts' => '1437405457.000002' }
+            ],
+            'has_more' => false }
+        end
+      end
+    end
+  end
+end
