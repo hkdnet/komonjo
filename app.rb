104af83a68a92bff5b5fe36f03442270212296f6
hkdnet <hkdnet@users.noreply.github.com>
2015-07-22 23:24:23 +0900

    use compass

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
 require 'slim/include'
+require 'compass'
 require_relative 'lib/mock/slack_mock'
 
 module Komonjo
@@ -9,6 +10,15 @@ module Komonjo
   class App < Sinatra::Base
     configure :development do
       register Sinatra::Reloader
+    end
+
+    Compass.configuration do |c|
+      c.project_path = File.dirname(__FILE__)
+      c.sass_dir = 'views/scss'
+    end
+
+    get '/css/:name.css' do
+      scss :"scss/#{params[:name]}"
     end
 
     get '/' do
