62f2118d25dbdfb4cd62082a74ef8983b5713dc8
hkdnet <hkdnet@users.noreply.github.com>
2015-07-22 22:40:18 +0900

    add sinatra/autoreload

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org/'
 
 gem 'sinatra', '1.4.6'
+gem 'sinatra-contrib'
 gem 'slack-api', '1.1.6'
 gem 'dotenv'
 gem 'slim'
--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'slim'
 require 'slim/include'
 require_relative 'lib/mock/slack_mock'
@@ -6,6 +7,10 @@ require_relative 'lib/mock/slack_mock'
 module Komonjo
   # routing
   class App < Sinatra::Base
+    configure :development do
+      register Sinatra::Reloader
+    end
+
     get '/' do
       @posts = Komonjo::Mock::SlackMock.chat_logs
       slim :index
