52c0fad228b1b3bd9602ca746a253483d22c174e
hkdnet <hkdnet@users.noreply.github.com>
2015-07-24 01:43:00 +0900

    refactor a complex method

--- lib/gateway/message_gateway.rb
+++ lib/gateway/message_gateway.rb
@@ -9,15 +9,19 @@ module Komonjo
 
       def messages
         @history.map do |h|
-          if h['user']
-            user = @users.find { |u| u['id'] == h['user'] }
-            fail "unknown user #{h['user']}" unless user
-            h['username'] = user['name']
-            h['icon_url'] = user['profile']['image_48']
-            puts h['user'], user, h['username']
-          end
+          return h unless h['user']
+          user = user h['user']
+          p h
+          h['username'] = user['name']
+          h['icon_url'] = user['profile']['image_48']
           h
         end
+      end
+
+      def user(user_id)
+        user = @users.find { |u| u['id'] == user_id }
+        fail "unknown user #{h['user']}" unless user
+        user
       end
     end
   end
