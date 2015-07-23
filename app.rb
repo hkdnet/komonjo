01abdeeb019ac944ae48a6be9db5b17a176c0976
hkdnet <hkdnet@users.noreply.github.com>
2015-07-23 22:22:25 +0900

    we can get logs with api_token and channel name

--- app.rb
+++ app.rb
@@ -4,12 +4,15 @@ require 'slim'
 require 'slim/include'
 require 'compass'
 require_relative 'lib/mock/slack_mock'
+require_relative 'lib/adapter/slack_adapter'
 
 module Komonjo
   # routing
   class App < Sinatra::Base
     configure :development do
       register Sinatra::Reloader
+      require 'dotenv'
+      Dotenv.load
     end
 
     Compass.configuration do |c|
@@ -22,7 +25,16 @@ module Komonjo
     end
 
     get '/' do
+      @api_token = ENV['TEST_API_TOKEN'] || ''
       @posts = Komonjo::Mock::SlackMock.chat_logs
+      slim :index
+    end
+
+    post '/' do
+      @api_token = params[:api_token]
+      @channel_name = params[:channel_name]
+      adapter = Komonjo::Adapter::SlackAdapter.new @api_token
+      @posts = adapter.all_logs_at @channel_name
       slim :index
     end
   end
