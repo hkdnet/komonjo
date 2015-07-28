8a99e8952ae5dba61b8e87428b7529510bbbd39f
hkdnet <hkdnet@users.noreply.github.com>
2015-07-29 01:39:28 +0900

    add Komonjo::Model::Profile test

---
+++ spec/model/profile_test.rb
@@ -0,0 +1,35 @@
+require_relative '../spec_helper'
+
+describe Komonjo::Model::Profile do
+  describe 'create' do
+    it 'should return an instance of User' do
+      h = {}
+      u = Komonjo::Model::Profile.create(h)
+      u.class.must_equal Komonjo::Model::Profile
+    end
+  end
+  describe 'attributes' do
+    ATTRS = %i(first_name last_name real_name email skype phone image_24
+               image_32 image_48 image_72 image_192 )
+    r = Random.new
+    ATTRS.each do |a|
+      it "should be able to read attr #{a}" do
+        h = {}
+        val = r.rand(1000).to_s
+        h[a] = val
+        u = Komonjo::Model::Profile.create(h)
+        fail "no method: #{a}" unless u.respond_to? a.to_s
+        u.send(a).must_equal val
+      end
+
+      it "should be able to write attr #{a}" do
+        h = {}
+        u = Komonjo::Model::Profile.create(h)
+        fail "no method: #{a}" unless u.respond_to?("#{a}=")
+        val = r.rand(1000)
+        u.send("#{a}=", val)
+        u.send(a).must_equal val
+      end
+    end
+  end
+end
