45d4e1761805823a0c9b5c54dc79106c1a24a323
hkdnet <hkdnet@users.noreply.github.com>
2015-07-29 01:33:08 +0900

    user model's attributes can be written

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,7 @@ require 'dotenv'
 Dir[File.expand_path('../../lib/connection', __FILE__) << '/*.rb'].each do |f|
   require f
 end
+Dir[File.expand_path('../../lib/model', __FILE__) << '/*.rb'].each do |f|
+  require f
+end
 Dotenv.load
