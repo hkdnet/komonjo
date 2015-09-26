dabfe9e934c732028f451ea23bd7f76559e56a7d
hkdnet <hkdnet@users.noreply.github.com>
2015-09-27 02:27:03 +0900

    WIP

--- public/scripts/reactify.js
+++ public/scripts/reactify.js
@@ -1,7 +1,7 @@
 var LoginButton = React.createClass({
   render: function() {
     return(
-      <button className="login-button" onClick={this.props.onClick}>
+      <button className="btn btn-default login-button" onClick={this.props.onClick}>
         Login
       </button>
     )
@@ -10,35 +10,47 @@ var LoginButton = React.createClass({
 var LoginRow = React.createClass({
   getInitialState: function() {
     client.login().then(function(d) {
-      this.setState({token: d.data});
+      this.setState(d);
     }.bind(this));
     return {token: ''};
   },
   handleClick: function(e) {
     e.preventDefault();
     toastr.info('Processing...', 'login');
-    var token = this.state.token || '';
+    var token = this.state.token;
     var afterLogin = this.props.afterLogin || function() {};
     client.login(token).then(function(d) {
       toastr.success('logged in', 'login');
       afterLogin();
     });
   },
+  handleChange: function(e) {
+    e.preventDefault();
+    this.setState({
+      data: e.target.value
+    });
+  },
   render: function() {
     return(
       <div>
-        <span>Slack API Token: </span>
-        <input ref="token" type="password" value={this.state.token}/>
+        <label htmlFor="token">Slack API Token: </label>
+        <input placeholder="slack api token" ref="token" type="password"
+          value={this.state.data} name="token" className="form-control"
+          onChange={this.handleChange} />
         <LoginButton onClick={this.handleClick} />
       </div>
     )
   }
 });
-
 var ChannelButton = React.createClass({
+  handleClick: function(e) {
+    this.props.afterSelectChannel({
+      channelName: this.props.channelName
+    });
+  },
   render: function() {
     return(
-      <li className="channel-button">
+      <li className="list-unstyled channel-button" onClick={this.handleClick} >
         <i className="fa fa-television" />
         <span>
           {this.props.channelName}
@@ -49,9 +61,10 @@ var ChannelButton = React.createClass({
 });
 var ChannelRow = React.createClass({
   render: function() {
+    var afterSelectChannel = this.props.afterSelectChannel;
     var channelButtons = this.props.channels.map(function(e) {
       return(
-        <ChannelButton channelName={e} />
+        <ChannelButton channelName={e} afterSelectChannel={afterSelectChannel} />
       )
     });
     return(
@@ -64,7 +77,7 @@ var ChannelRow = React.createClass({
 var MenuColumn = React.createClass({
   getChannels: function() {
     client.channels().then(function(d) {
-      this.setState({data : d.data});
+      this.setState(d);
     }.bind(this));
   },
   getInitialState: function() {
@@ -72,33 +85,99 @@ var MenuColumn = React.createClass({
   },
   render: function() {
     return(
-      <div className="menu-column">
+      <div className="col-md-3 column menu-column">
         <LoginRow afterLogin={this.getChannels}/>
-        <ChannelRow channels={this.state.data} />
+        <ChannelRow channels={this.state.data} afterSelectChannel={this.props.afterSelectChannel}/>
       </div>
     );
+  }
+});
+var MessageIcon = React.createClass({
+  render: function() {
+    return (
+      <div className="col-md-2 message-icon">
+        <img className="img-rounded" src={this.props.src} />
+      </div>
+    )
+  }
+});
+var MessageHeader = React.createClass({
+  render: function() {
+    return(
+      <div className="message-header row">
+        <div className="message-username col-md-4">{this.props.name}</div>
+        <div className="message-timestamp col-md-8">{this.props.ts}</div>
+      </div>
+    )
+  }
+})
+var Message = React.createClass({
+  render: function() {
+    return (
+      <div className="message" data-md="">
+        <MessageIcon src="https://avatars.slack-edge.com/2015-07-20/7916335493_befb8f27e45d5ccade4b_48.jpg" />
+        <div className="col-md-6">
+          <MessageHeader name="name001" ts="70/01/01 00:00:00"/>
+          <div className="row message-text">
+            <pre className="col-md-12">Hello, world</pre>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+var Messages = React.createClass({
+  getMessagesByChannelName: function(channelName) {
+    client.messages(channelName).done(function (d){
+      this.setState(d);
+    })
+  },
+  render: function() {
+    var arr = [0, 1, 2];
+    var messages = arr.map(function(e){
+      return (
+        <Message />
+      )
+    });
+    return(
+      <div className="messages">
+        {messages}
+      </div>
+    )
   }
 });
 var ViewerColumn = React.createClass({
   render: function () {
     return(
-      <div className="viwer-column">
+      <div className="col-md-9 column viewer-column">
+        <h2>
+          {this.props.channelName}
+        </h2>
+        <Messages />
       </div>
     )
   }
 })
 var KomonjoScreen = React.createClass({
+  getInitialState: function() {
+    return {
+      channelName: ''
+    };
+  },
+  afterSelectChannel: function(d) {
+    this.setState(d);
+  },
   render: function() {
     return(
-      <div className="komonjo-screen">
-        <MenuColumn />
-        <ViewerColumn />
+      <div className="row komonjo-screen">
+        <MenuColumn afterSelectChannel={this.afterSelectChannel} />
+        <ViewerColumn channelName={this.state.channelName} />
       </div>
     )
   }
 })
 
 React.render(
-  <MenuColumn />,
+  <KomonjoScreen />,
   document.getElementById('react')
 )
